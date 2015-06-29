bd5e7f20504b20ae13b73a9bacd14622f1ca1d69
Cardenio <tong.lyu@hotmail.com>
2015-06-29 10:00:06 +0800

    Update RackAware.py

--- RackAware.py
+++ RackAware.py
@@ -2,14 +2,14 @@
 #-*-coding:utf-8 -*-
 import sys
 rack = {
-"node1":"rack1",
-"node2":"rack2",
-"node3":"rack3",
-"node4":"rack4",
-"222.198.132.209":"rack1",
-"222.198.132.208":"rack2",
-"222.198.132.207":"rack3",
-"222.198.132.210":"rack4"
+  "node1":"rack1",
+  "node2":"rack2",
+  "node3":"rack3",
+  "node4":"rack4",
+  "222.198.132.209":"rack1",
+  "222.198.132.208":"rack2",
+  "222.198.132.207":"rack3",
+  "222.198.132.210":"rack4"
 }
 
 if __name__=="__main__":
